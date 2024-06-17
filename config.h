aa5572d87eeb5ff5b7d987f3e126ad9b59c91392
Aaron John <unrealapex@disroot.org>
2024-06-16 20:00:49 -0700

    feat: add movecenter patch

--- config.h
+++ config.h
@@ -193,6 +193,7 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY,                       XK_c,      movecenter,     {0} },
 };
 
 /* button definitions */
--- dwm.c
+++ dwm.c
@@ -198,6 +198,7 @@ static void motionnotify(XEvent *e);
 static void moveresize(const Arg *arg);
 static void moveresizeedge(const Arg *arg);
 static void movemouse(const Arg *arg);
+static void movecenter(const Arg *arg);
 static Client *nexttiled(Client *c);
 static void pop(Client *c);
 static void propertynotify(XEvent *e);
@@ -1457,6 +1458,14 @@ moveresizeedge(const Arg *arg) {
 		if ((msx + nmx) > c->x && (msy + nmy) > c->y)
 			XWarpPointer(dpy, None, None, 0, 0, 0, 0, nmx, nmy);
 	}
+}
+
+void
+movecenter(const Arg *arg)
+{
+	selmon->sel->x = selmon->sel->mon->mx + (selmon->sel->mon->mw - WIDTH(selmon->sel)) / 2;
+	selmon->sel->y = selmon->sel->mon->my + (selmon->sel->mon->mh - HEIGHT(selmon->sel)) / 2;
+	arrange(selmon);
 }
 
 Client *
